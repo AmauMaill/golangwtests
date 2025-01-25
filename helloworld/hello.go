88950af6e73da641cd10a61025e30e571094824a
Amaury <amaury.maill@gmail.com>
2025-01-25 19:06:06 +0100

    - Passing test for injecting name - Supporting name input (or default value) - Supporting multiple languages - Added in self-contained module

---
+++ helloworld/hello.go
@@ -0,0 +1,32 @@
+package helloworld
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		return spanishHelloPrefix
+	case french:
+		return frenchHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
